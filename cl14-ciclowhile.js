e4940f863f2ec1b84ae6f0fb4aa1aa6c91726b2a
silvermiguel96@gmail.com <silvermiguel96@gmail.com>
2018-11-11 07:23:07 -0500

    14-15-16

--- cl14-ciclowhile.js
+++ cl14-ciclowhile.js
@@ -10,15 +10,24 @@ console.log(`Al inicio del año ${persona.nombre} pesa ${persona.peso}`)
 const INCREMENTO_PESO = 0.2
 const DIAS_DEL_AÑO = 365
 
-var Incremento = persona => persona.peso += INCREMENTO_PESO
+var incremento = persona => persona.peso += INCREMENTO_PESO
 var decremento = persona => persona.peso -= INCREMENTO_PESO
 
-for(let i = 1;i < DIAS_DEL_AÑO ;i += 1){
-    let Nrandom = Math.random()
+var comeMucho = () => Math.random() < 0.3
+var ejercicio = () => Math.random() < 0.4
 
-
+const META = persona.peso - 3
+var dias = 0
+while (persona.peso > META){
+    if(comeMucho()){
+        incremento(persona)
+    }
+    if(ejercicio()){
+        decremento(persona)
+    }
+    dias += 1
 }
-console.log(`Al final del año ${persona.nombre} pesa ${persona.peso.toFixed(2)}`)
+console.log(`${persona.nombre} de demoro ${dias} en lograr su meta de ${META}`)
 }
 
 IncrementarPesoAnual(miguelangel)
